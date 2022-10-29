263eab890ceb29cd3b75f6c3b47f33ad433057e6
NandoMCT <nandomct13@gmail.com>
2022-10-29 11:09:25 -0300

    Update Classe Atendente.java
    Criaçao dos Getter e Setters da classe Atendente.

--- src/entities/Atendente.java
+++ src/entities/Atendente.java
@@ -1,5 +1,14 @@
 package entities;
 
 public class Atendente extends Funcionario {
+	private int quartosAlugados;
+
+	public int getQuartosAlugados() {
+		return quartosAlugados;
+	}
+
+	public void setQuartosAlugados(int quartosAlugados) {
+		this.quartosAlugados = quartosAlugados;
+	}
 	
 }
